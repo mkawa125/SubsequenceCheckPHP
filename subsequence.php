61392f5cfb6c2e5355b0e094ee776a3e8a07717e
Dahabu Saidi <dahabusaidi@gmail.com>
2021-09-15 08:49:01 +0300

    Code refactor

--- subsequence.php
+++ subsequence.php
@@ -6,22 +6,20 @@ A subsequence is a sequence that can be derived from another sequence by deletin
 Expected time complexity is linear. 
 */
 
-// Iterative PHP program to check if a string is subsequence of another string
-
-// Returns true if string1[] is a subsequence of string2[].
-// lenthOfString1 is length of string1 and lenthOfString2 is length of string2
+/* 
+Iterative PHP program to check if a string is subsequence of another string
+Returns true if string1[] is a subsequence of string2[].
+lenthOfString1 is length of string1 and lenthOfString2 is length of string2
+*/
 
 function isSubSequence($string1, $string2, $lenthOfString1, $lenthOfString2)
 {
 	// For index of string1
 	$j = 0;
 	
-	/* Traverse string2 and string1,
-	and compare current
-	character of string2 with
-	first unmatched char of
-	string1, if matched then
-    move ahead in string1
+    /* 
+    Traverse string2 and string1, and compare current character of string2 with first unmatched char of string1, 
+    if matched then move ahead in string1
     */
 	for($i = 0; $i < $lenthOfString2 and $j < $lenthOfString1; $i++){
         if ($string1[$j] == $string2[$i]){
